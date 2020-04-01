da3b1a9ff0b0c313d94722cdd8c8315fbf11d8c1
yoogj0513 <yoogj0513@naver.com>
2020-04-01 09:41:11 +0900

    sql test

--- Scripts/test.sql
+++ Scripts/test.sql
@@ -15,3 +15,10 @@ select stud_id, name, email, dob, phone,
 		from students where stud_id = 1;
 
 select stud_id as studId, name, email, phone, dob from students;
+
+desc students;
+
+insert into students (stud_id , name , email , phone , dob ) 
+values(3, '홍길동', 'test@test.co.kr', '010-1234-1234', '1990-03-28');
+
+delete from students where stud_id = 3;
